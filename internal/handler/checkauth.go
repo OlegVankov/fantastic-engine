aee74e14491d5e2da1e765c8293c1471cc826139
oleg <zelhat@gmail.com>
2024-01-06 17:47:32 +0300

    refactor midlleware

---
+++ internal/handler/checkauth.go
@@ -0,0 +1,45 @@
+package handler
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v4"
+
+	"github.com/OlegVankov/fantastic-engine/internal/util"
+)
+
+func Auth(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		auth := strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ")
+
+		if auth == "" {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		userClaim := util.UserClaim{}
+
+		token, err := jwt.ParseWithClaims(auth, &userClaim, func(token *jwt.Token) (interface{}, error) {
+			return []byte("secret_key"), nil
+		})
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		if !token.Valid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		if _, ok := Users2[userClaim.Username]; !ok {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		r.Header.Add("username", userClaim.Username)
+		h.ServeHTTP(w, r)
+	})
+}
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -6,10 +6,7 @@ import (
 	"io"
 	"net/http"
 	"sort"
-	"strings"
 	"time"
-
-	"github.com/golang-jwt/jwt/v4"
 
 	"github.com/OlegVankov/fantastic-engine/internal/util"
 )
@@ -227,39 +224,4 @@ func Withdrawals(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(withdrawals)
-}
-
-func Auth(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		auth := strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ")
-
-		if auth == "" {
-			w.WriteHeader(http.StatusUnauthorized)
-			return
-		}
-
-		userClaim := util.UserClaim{}
-
-		token, err := jwt.ParseWithClaims(auth, &userClaim, func(token *jwt.Token) (interface{}, error) {
-			return []byte("secret_key"), nil
-		})
-
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		if !token.Valid {
-			w.WriteHeader(http.StatusUnauthorized)
-			return
-		}
-
-		if _, ok := Users2[userClaim.Username]; !ok {
-			w.WriteHeader(http.StatusUnauthorized)
-			return
-		}
-
-		r.Header.Add("username", userClaim.Username)
-		h.ServeHTTP(w, r)
-	})
 }
